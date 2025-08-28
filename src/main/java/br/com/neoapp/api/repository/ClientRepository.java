9dd408fcddd034f985bfc47039caf5c9a53b965c
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-28 20:39:45 -0300

    feat: adiciona metodos de verificacao de unicidade para cliente
    Adiciona os métodos `existsByEmail` e `existsByCpf` na interface `ClientRepository`.

--- src/main/java/br/com/neoapp/api/repository/ClientRepository.java
+++ src/main/java/br/com/neoapp/api/repository/ClientRepository.java
@@ -1,11 +1,15 @@
 package br.com.neoapp.api.repository;
 
 import br.com.neoapp.api.model.Client;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByCpf(String cpf);
 }
