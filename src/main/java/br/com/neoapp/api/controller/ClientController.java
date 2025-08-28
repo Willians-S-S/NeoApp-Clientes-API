64b08f4d5d4449ccc9dbf0fe3be520fb090ba33b
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-28 20:43:57 -0300

    feat: implementa o endpoint POST para criacao de cliente
    Adiciona o endpoint `POST /api/v1/clients` no `ClientController`. Este endpoint processa a requisição, salva o cliente no banco de dados e retorna uma resposta `201 Created` com a URI do novo recurso.

--- src/main/java/br/com/neoapp/api/controller/ClientController.java
+++ src/main/java/br/com/neoapp/api/controller/ClientController.java
@@ -1,14 +1,36 @@
 package br.com.neoapp.api.controller;
 
+import br.com.neoapp.api.controller.dto.ClientRequestDTO;
+import br.com.neoapp.api.controller.dto.ClientResponseDTO;
 import br.com.neoapp.api.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping(name = "/api/v1/clients")
+@RequestMapping(value = "/api/v1/clients")
 public class ClientController {
 
     @Autowired
     private ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> creatClient(@RequestBody ClientRequestDTO clientRequestDTO){
+
+        ClientResponseDTO clientResponseDTO = clientService.creatClient(clientRequestDTO);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(clientResponseDTO.id())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(clientResponseDTO);
+    }
+
 }
