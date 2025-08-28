7b7d574c865c2ddabddc7ca986fdb1dc17ed6ab6
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-27 23:58:42 -0300

    feat: cria o repositório para cliente

---
+++ src/main/java/br/com/neoapp/api/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package br.com.neoapp.api.repository;
+
+import br.com.neoapp.api.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+}
