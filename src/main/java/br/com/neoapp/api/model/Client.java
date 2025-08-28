9a77111e27d5032429cd9b932e1e89ac14ca0011
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-28 19:12:35 -0300

    feat: adiciona validacoes de dados na entidade cliente
    Adiciona anotações de validação (`@NotBlank`, `@Size`, `@Email` e `@Past`) e mapeamento (`@Column`) na entidade `Client` para garantir a integridade dos dados antes de serem persistidos no banco de dados.

--- src/main/java/br/com/neoapp/api/model/Client.java
+++ src/main/java/br/com/neoapp/api/model/Client.java
@@ -1,7 +1,14 @@
 package br.com.neoapp.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
@@ -18,12 +25,39 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @NotBlank(message = "O nome não pode ser vazio.")
+    @Size(min = 2, max = 100)
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Past(message = "A data de nascimento deve ser no passado.")
+    @Column(nullable = false)
     private LocalDate birthday;
+
+    @NotBlank
+    @Email(message = "Formato de e-mail inválido.")
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres.")
+    @Column(nullable = false)
     private String password;
+
+    @Column(length = 20)
     private String phone;
+
+    @NotBlank
+    @CPF(message = "CPF inválido.")
+    @Column(nullable = false, unique = true, length = 11)
     private String cpf;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private OffsetDateTime creatAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
     private OffsetDateTime updateAt;
 }
