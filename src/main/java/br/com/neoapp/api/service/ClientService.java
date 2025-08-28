2ef4e7b2542ee771da5cd45a1a95384c4be465d6
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-28 00:47:59 -0300

    feat: implementa a conversao de DTO para entidade na camada de servico
    Adiciona o método `clientRequestDTOToClient` na classe `ClientService`. Este método converte um objeto
    `ClientRequestDTO` em uma entidade `Client`, encapsulando a lógica de criação e garantindo que
    os dados sejam mapeados corretamente.

--- src/main/java/br/com/neoapp/api/service/ClientService.java
+++ src/main/java/br/com/neoapp/api/service/ClientService.java
@@ -1,12 +1,31 @@
 package br.com.neoapp.api.service;
 
+import br.com.neoapp.api.controller.dto.ClientRequestDTO;
+import br.com.neoapp.api.controller.dto.ClientResponseDTO;
+import br.com.neoapp.api.model.Client;
 import br.com.neoapp.api.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
 
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository clientRepository;
+
+    
+
+    public Client requestDTOToClient(ClientRequestDTO clientRequestDTO){
+        Client client = new Client();
+        client.setName(clientRequestDTO.name());
+        client.setEmail(clientRequestDTO.email());
+        client.setCpf(clientRequestDTO.cpf());
+        client.setPhone(clientRequestDTO.phone());
+        client.setPassword(clientRequestDTO.password());
+        client.setBirthday(clientRequestDTO.birthday());
+        client.setCreatAt(OffsetDateTime.now());
+        client.setUpdateAt(OffsetDateTime.now());
+    }
 }
