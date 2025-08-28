e759289e7e91aaa6cb5a183dfbb5d1272f274f77
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-28 00:56:35 -0300

    feat: implementa a conversao de entidade para DTO
    Adiciona o método `clientToResponseDTO` na classe `ClientService`. Este método converte um objeto
    `Client` em um `ClientResponseDTO`, calculando a idade a partir da data de nascimento e
    expondo apenas os dados necessários na resposta da API.

--- src/main/java/br/com/neoapp/api/service/ClientService.java
+++ src/main/java/br/com/neoapp/api/service/ClientService.java
@@ -7,7 +7,9 @@ import br.com.neoapp.api.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.Period;
 
 @Service
 public class ClientService {
@@ -15,7 +17,7 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    
+
 
     public Client requestDTOToClient(ClientRequestDTO clientRequestDTO){
         Client client = new Client();
@@ -27,5 +29,21 @@ public class ClientService {
         client.setBirthday(clientRequestDTO.birthday());
         client.setCreatAt(OffsetDateTime.now());
         client.setUpdateAt(OffsetDateTime.now());
+    }
+
+    public ClientResponseDTO clientToResponseDTO(Client client){
+
+        Integer age = Period.between(client.getBirthday(), LocalDate.now()).getYears();
+
+        return new ClientResponseDTO(client.getId(),
+                client.getName(),
+                age,
+                client.getEmail(),
+                client.getPhone(),
+                client.getCpf(),
+                client.getCreatAt(),
+                client.getUpdateAt()
+                );
+
     }
 }
