cbf988102d700d7b6b604c67d5441a3fa8808451
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-28 14:26:45 -0300

    refactor: migra conversao de DTO para a classe ClientMapper
    Remove a lógica de conversão de DTOs da classe `ClientService` e a move para a nova classe `ClientMapper`.
    Essa refatoração melhora a separação de responsabilidades e torna o `ClientService` focado apenas na lógica de negócio.

--- src/main/java/br/com/neoapp/api/service/ClientService.java
+++ src/main/java/br/com/neoapp/api/service/ClientService.java
@@ -2,6 +2,7 @@ package br.com.neoapp.api.service;
 
 import br.com.neoapp.api.controller.dto.ClientRequestDTO;
 import br.com.neoapp.api.controller.dto.ClientResponseDTO;
+import br.com.neoapp.api.mapper.ClientMapper;
 import br.com.neoapp.api.model.Client;
 import br.com.neoapp.api.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.Period;
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -17,35 +19,8 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Autowired
+    private ClientMapper clientMapper;
+    
 
-
-    public Client requestDTOToClient(ClientRequestDTO clientRequestDTO){
-        Client client = new Client();
-        client.setName(clientRequestDTO.name());
-        client.setEmail(clientRequestDTO.email());
-        client.setCpf(clientRequestDTO.cpf());
-        client.setPhone(clientRequestDTO.phone());
-        client.setPassword(clientRequestDTO.password());
-        client.setBirthday(clientRequestDTO.birthday());
-        client.setCreatAt(OffsetDateTime.now());
-        client.setUpdateAt(OffsetDateTime.now());
-
-        return  client;
-    }
-
-    public ClientResponseDTO clientToResponseDTO(Client client){
-
-        Integer age = Period.between(client.getBirthday(), LocalDate.now()).getYears();
-
-        return new ClientResponseDTO(client.getId(),
-                client.getName(),
-                age,
-                client.getEmail(),
-                client.getPhone(),
-                client.getCpf(),
-                client.getCreatAt(),
-                client.getUpdateAt()
-                );
-
-    }
 }
