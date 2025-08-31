c7d9c1837feb6856ff02ea0acc83b225a46c7ae7
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-31 00:54:48 -0300

    docs: documenta o endpoint de criacao de cliente com OpenAPI

--- src/main/java/br/com/neoapp/api/controller/ClientController.java
+++ src/main/java/br/com/neoapp/api/controller/ClientController.java
@@ -2,17 +2,28 @@ package br.com.neoapp.api.controller;
 
 import br.com.neoapp.api.controller.dto.ClientRequestDTO;
 import br.com.neoapp.api.controller.dto.ClientResponseDTO;
+import br.com.neoapp.api.exceptions.StandardError;
+import br.com.neoapp.api.exceptions.ValidationError;
 import br.com.neoapp.api.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
+@Tag(name = "Clientes", description = "Endpoints para o gerenciamento de clientes")
 @RestController
 @RequestMapping(value = "/api/v1/clients")
 public class ClientController {
@@ -20,6 +31,25 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @Operation(
+            summary = "Criar um novo cliente",
+            description = "Cria um novo cliente no sistema com base nos dados fornecidos. " +
+                    "Após a criação, retorna o status 201 e a localização do novo recurso."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Cliente criado com sucesso",
+                    content = { @Content(schema = @Schema(implementation = ClientResponseDTO.class), mediaType = "application/json") },
+                    headers = { @Header(name = "Location", description = "URI para acessar o cliente recém-criado") }
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Conflito: O e-mail ou CPF informado já está cadastrado no sistema.",
+                    content = {@Content(schema = @Schema(implementation = StandardError.class)) }
+            ),
+            @ApiResponse(responseCode = "422", description = "Dados inválidos fornecidos", content = { @Content(schema = @Schema(implementation = ValidationError.class)) }),
+    })
     @PostMapping
     public ResponseEntity<ClientResponseDTO> creatClient(@RequestBody @Valid ClientRequestDTO clientRequestDTO){
 
