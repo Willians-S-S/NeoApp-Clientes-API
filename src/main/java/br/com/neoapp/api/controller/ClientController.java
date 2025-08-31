bb63672ba6eaa8e3d075b436b8769010c8832626
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-31 02:22:56 -0300

    feat: adiciona endpoint para listagem paginada de clientes

--- src/main/java/br/com/neoapp/api/controller/ClientController.java
+++ src/main/java/br/com/neoapp/api/controller/ClientController.java
@@ -14,11 +14,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -62,6 +61,11 @@ public class ClientController {
         return ResponseEntity
                 .created(location)
                 .body(clientResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientResponseDTO>> getAllClientsPageable(Pageable pageable){
+        return ResponseEntity.ok().body(clientService.getAllClientsPageable(pageable));
     }
 
 }
