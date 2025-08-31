98b1cc7382e40022d06054c4794ee74314b0b580
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-08-31 00:59:13 -0300

    docs: adiciona documentacao Javadoc para o metodo de criacao de cliente

--- src/main/java/br/com/neoapp/api/service/ClientService.java
+++ src/main/java/br/com/neoapp/api/service/ClientService.java
@@ -19,6 +19,23 @@ public class ClientService {
     @Autowired
     private ClientMapper clientMapper;
 
+    /**
+     * Cria um novo cliente no sistema a partir dos dados fornecidos.
+     *
+     * <p>Este método executa as seguintes etapas:</p>
+     * <ol>
+     * <li><b>Validação de E-mail:</b> Verifica se o e-mail fornecido já existe no repositório.</li>
+     * <li><b>Validação de CPF:</b> Verifica se o CPF fornecido já existe no repositório.</li>
+     * <li><b>Mapeamento e Persistência:</b> Se as validações passarem, o DTO de requisição é convertido para uma entidade {@code Client}.</li>
+     * <li><b>Salvamento:</b> A nova entidade de cliente é salva no banco de dados.</li>
+     * <li><b>Retorno:</b> A entidade salva é mapeada para um DTO de resposta e retornada.</li>
+     * </ol>
+     *
+     * @param clientRequestDTO O objeto de transferência de dados (DTO) que contém as informações do novo cliente a ser criado.
+     * @return Um {@code ClientResponseDTO} contendo os dados do cliente recém-criado, incluindo seu ID gerado pelo banco de dados.
+     * @throws EmailExistsException Se o e-mail fornecido em {@code clientRequestDTO} já estiver registrado no sistema.
+     * @throws CpfExistsException Se o CPF fornecido em {@code clientRequestDTO} já estiver registrado no sistema.
+     */
     public ClientResponseDTO creatClient(ClientRequestDTO clientRequestDTO) {
         if (clientRepository.existsByEmail(clientRequestDTO.email())){
             throw new EmailExistsException("O endereço de e-mail informado já está registrado.");
