97ae2e52efa8447792014bd21fb8d5eb5780c7b2
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-09-01 19:12:00 -0300

    feat: adiciona configuração de tokens JWT
    Adiciona a configuração de JWT na classe `ApplicationConfig`. Isso inclui a injeção das chaves RSA pública e privada, a criação de um `JwtDecoder` para decodificar tokens e um `JwtEncoder` para gerar tokens, preparando a aplicação para autenticação baseada em JWT.

--- src/main/java/br/com/neoapp/api/config/ApplicationConfig.java
+++ src/main/java/br/com/neoapp/api/config/ApplicationConfig.java
@@ -1,15 +1,43 @@
 package br.com.neoapp.api.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 public class ApplicationConfig {
 
+    @Value("${jwt.public.key}")
+    private RSAPublicKey key;
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey priv;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    JwtDecoder decoder(){
+        return NimbusJwtDecoder.withPublicKey(key).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        var jwk = new RSAKey.Builder(key).privateKey(priv).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
