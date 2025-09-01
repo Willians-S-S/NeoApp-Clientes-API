eb1cfac28f46962b599d74ad37f889e928d8aa69
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-09-01 00:15:56 -0300

    feat: adiciona método de busca paginada por atributos no repositório

--- src/main/java/br/com/neoapp/api/repository/ClientRepository.java
+++ src/main/java/br/com/neoapp/api/repository/ClientRepository.java
@@ -1,15 +1,26 @@
 package br.com.neoapp.api.repository;
 
 import br.com.neoapp.api.model.Client;
-import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.time.LocalDate;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, String> {
     boolean existsByEmail(String email);
     boolean existsByCpf(String cpf);
+
+    @Query("SELECT c FROM Client c WHERE " +
+            "(:name IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))) AND " +
+            "(:email IS NULL OR LOWER(c.email) LIKE LOWER(CONCAT('%', :email, '%'))) AND " +
+            "(:cpf IS NULL OR c.cpf = :cpf) AND " +
+            "(:phone IS NULL OR c.phone LIKE CONCAT('%', :phone, '%')) AND " +
+            "(:birthdayStart IS NULL OR c.birthday >= :birthdayStart) AND " +
+            "(:birthdayEnd IS NULL OR c.birthday <= :birthdayEnd)"
+    )
+    Page<Client> getAllClientsWithAttributesPage(String name, String email, String cpf, String phone, LocalDate birthdayStart, LocalDate birthdayEnd, Pageable pageable);
 }
