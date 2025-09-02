75442438e06142beb46a7bb47f7afc4cb27babec
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-09-02 11:38:31 -0300

    docs: Documenta o enum RoleName

--- src/main/java/br/com/neoapp/api/enums/RoleName.java
+++ src/main/java/br/com/neoapp/api/enums/RoleName.java
@@ -1,5 +1,9 @@
 package br.com.neoapp.api.enums;
 
+/**
+ * Enum que define os papéis (roles) de autorização disponíveis no sistema.
+ * Utilizado para controlar o acesso a diferentes recursos da API.
+ */
 public enum RoleName {
     ADMIN,
     USER
