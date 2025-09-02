df4f5197227429864cb2a3289a04bc65d713f681
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-09-01 23:15:37 -0300

    fix: Altera método do endpoint de login para POST

--- src/main/java/br/com/neoapp/api/controller/AuthController.java
+++ src/main/java/br/com/neoapp/api/controller/AuthController.java
@@ -17,7 +17,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -31,7 +34,7 @@ public class AuthController {
     @Autowired
     private ClientService clientService;
 
-    @GetMapping(value = "/login")
+    @PostMapping(value = "/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
         return ResponseEntity.ok()
                 .body(authenticationService.authenticate(loginRequest));
